7809fc027b302865e316c0dd9b1c4a2b71133310
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-09-25 12:26:19 +0100

    import reactive forms module to departures module

--- src/app/departures/departures.module.ts
+++ src/app/departures/departures.module.ts
@@ -2,6 +2,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 // Other Modules
 import { GoogleMapsModule } from '@angular/google-maps'
@@ -13,19 +14,22 @@ import { MainComponent } from './components/main/main.component';
 // Services
 import { ExternalApisService } from './transport-api.service';
 import { MapComponent } from './components/main/map/map.component';
-import { DeparturesListingsComponent } from './components/main/departures-listings/departures-listings.component'
+import { DeparturesListingsComponent } from './components/main/departures-listings/departures-listings.component';
+import { SearchBoxComponent } from './components/main/search-box/search-box.component'
 
 @NgModule({
   declarations: [
     HeaderComponent,
     MainComponent,
     MapComponent,
-    DeparturesListingsComponent
+    DeparturesListingsComponent,
+    SearchBoxComponent
   ],
   imports: [
     CommonModule,
     HttpClientModule,
-    GoogleMapsModule
+    GoogleMapsModule,
+    ReactiveFormsModule
   ],
   exports: [
     HeaderComponent,
